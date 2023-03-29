d625bfc591b292fdc9cff4dfbc87500e1e4c768c
Thearith <thearith.s@ltlabs.co>
2023-03-29 16:29:32 +0700

    Update sample of two-way data binding

--- app/src/main/java/com/sample/databinding/TwoWaysDataBindingActivity.kt
+++ app/src/main/java/com/sample/databinding/TwoWaysDataBindingActivity.kt
@@ -9,22 +9,23 @@ import com.sample.databinding.databinding.Activity2waysDataBindingBinding
 class TwoWaysDataBindingActivity : AppCompatActivity() {
 
     private lateinit var binding: Activity2waysDataBindingBinding
+    private val user = User("Thearith", "thearith@gmail.com")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_2ways_data_binding)
+        binding.user = user
         setupListener()
     }
 
     private fun setupListener() {
-        binding.btnCopy.setOnClickListener {
-            if (binding.valueA.isNullOrEmpty()) {
-                Toast.makeText(this, "Please input A", Toast.LENGTH_SHORT).show()
-                return@setOnClickListener
-            }
-
-            binding.valueB = binding.valueA
-
+        binding.btnPrint.setOnClickListener {
+            println(user)
         }
     }
 }
+
+data class User(
+    var name: String,
+    var email: String
+)
